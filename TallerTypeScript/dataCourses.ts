b97a34c06a0036c1bfddfa3e96fc3fa39f92ef63
JuanMottax <jr.diaz@uniandes.edu.co>
2021-04-18 19:40:54 -0500

    TODO

---
+++ TallerTypeScript/dataCourses.ts
@@ -0,0 +1,10 @@
+
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("InfraComp", "Harold Castro", 4),
+  new Course("Culturas visuales", "Mauricio Rodas", 2),
+  new Course("Algoritmos", "Carlos Fuentes", 2),
+  new Course("Japones 4", "Hiromu Kondo", 1),
+  new Course("Futbol 2", "James R", 6)
+]
---
+++ TallerTypeScript/scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("InfraComp", "Harold Castro", 4),
+    new Course("Culturas visuales", "Mauricio Rodas", 2),
+    new Course("Algoritmos", "Carlos Fuentes", 2),
+    new Course("Japones 4", "Hiromu Kondo", 1),
+    new Course("Futbol 2", "James R", 6)
+];
